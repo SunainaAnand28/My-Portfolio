ef8802db2903800717ad3fbaff74fcea5d13b24a
Sunaina-Anand <161228634+user895naina@users.noreply.github.com>
2024-09-16 12:37:01 +0530

    Reverted the change

--- my-portfolio/src/index.js
+++ my-portfolio/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter as Router } from "react-router-dom";
 
 import './index.css';
 import App from './App';
@@ -9,9 +8,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
-   <Router>
      <App />
-   </Router>
 
 );
 
