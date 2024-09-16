fa6b90838e8c0b12c076ca9ac89be4455b467040
Sunaina-Anand <161228634+user895naina@users.noreply.github.com>
2024-09-16 12:17:18 +0530

    Wrapped the app with hash router

--- my-portfolio/src/index.js
+++ my-portfolio/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HashRouter as Router } from "react-router-dom";
+
 import './index.css';
 import App from './App';
 
@@ -7,7 +9,9 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
-    <App />
+   <Router>
+     <App />
+   </Router>
 
 );
 
