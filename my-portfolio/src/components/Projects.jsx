feab7ab1c23323e177d97861bdd41d49febf252a
Sunaina-Anand <161228634+user895naina@users.noreply.github.com>
2024-11-24 18:58:27 +0530

    Removed the project- working on error

--- my-portfolio/src/components/Projects.jsx
+++ my-portfolio/src/components/Projects.jsx
@@ -24,13 +24,13 @@ const Projects = () => {
       githubLink: "https://github.com/sunainaanand28/To-Do-Web-App",
       liveLink: "https://sunainaanand28.github.io/To-Do-Web-App/"
     },
-    {
-      title: "Forecastify",
-      description: "A weather application that allows users to check weather in their locality.",
-      techStack: "React, CSS",
-      githubLink: "https://github.com/sunainaanand28/forecastify",
-      liveLink: "https://forecastify-dun.vercel.app/"
-    }
+    // {
+    //   title: "Forecastify",
+    //   description: "A weather application that allows users to check weather in their locality.",
+    //   techStack: "React, CSS",
+    //   githubLink: "https://github.com/sunainaanand28/forecastify",
+    //   liveLink: "https://forecastify-dun.vercel.app/"
+    // }
     // will add more projects here...
   ];
 
